85c8836814b5d8d51a4e9c17f06b1b4f4f0fc3e8
Yuriichuk-Olha <olga.kantemyr@gmail.com>
2022-10-20 20:07:44 +0200

    replaced js

--- js/button.js
+++ js/button.js
@@ -1,54 +1,18 @@
-// (() => {
-//     const mobileMenu = document.querySelector('.js-menu-container');
-//     const openMenuBtn = document.querySelector('.js-open-menu');
-//     const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-//     const bodyScrollLock = document.querySelector('body');
-
-//     function toggleMenu ()  {
-//       const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//       mobileMenu.classList.toggle('is-open');
-
-//       bodyScrollLock.classList.toggle('no-scroll');
-  
-//       const scrollLockMethod = !isMenuOpen
-//         ? 'disableBodyScroll'
-//         : 'enableBodyScroll';
-//       bodyScrollLock[scrollLockMethod]("no-scroll");
-//     };
-  
-//     openMenuBtn.addEventListener('click', toggleMenu);
-//     closeMenuBtn.addEventListener('click', toggleMenu);
-  
-//     // Close the mobile menu on wider screens if the device orientation changes
-//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//       if (!e.matches) return;
-//       mobileMenu.classList.remove('is-open');
-//       openMenuBtn.setAttribute('aria-expanded', false);
-//       bodyScrollLock.enableBodyScroll('no-scroll');
-//     });
-//   })();
-
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const bodyScrollLock = document.querySelector('body');
-
-  // const toggleMenu = () => {
-    function toggleMenu() {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    bodyScrollLock.classLost.toggle('no-scroll');
-
-    // const scrollLockMethod = !isMenuOpen
-    //   ? 'disableBodyScroll'
-    //   : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -59,7 +23,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    // bodyScrollLock.enableBodyScroll(document.body);
-    bodyScrollLock.classLost.remove('no-scroll');
+    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
